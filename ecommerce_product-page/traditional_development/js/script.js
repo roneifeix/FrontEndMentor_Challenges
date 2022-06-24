b542b341fe110255193a30b3b41b46c726ecc507
roneifeix <roneifeix@gmail.com>
2022-06-24 12:10:18 -0300

    first content upload

---
+++ ecommerce_product-page/traditional_development/js/script.js
@@ -0,0 +1,19 @@
+function showNavBar() {
+    var navMenu = document.getElementById('nav-menu');
+    navMenu.style.display = 'block';
+}
+
+function hiddenNavBar() {
+    var navMenu = document.getElementById('nav-menu');
+    navMenu.style.display = 'none';
+}
+
+function showCart() {
+    var cartContainer = document.querySelector('header');
+    cartContainer.style.display = 'block';
+}
+
+function hiddenCart() {
+    var cartContainer = document.querySelector('header');
+    cartContainer.style.display = 'none';
+}
