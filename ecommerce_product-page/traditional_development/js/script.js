fde6bda9ba5c65e55c8d429384cfabacc736387a
roneifeix <roneifeix@gmail.com>
2022-06-24 14:53:59 -0300

    Update content

--- ecommerce_product-page/traditional_development/js/script.js
+++ ecommerce_product-page/traditional_development/js/script.js
@@ -1,3 +1,16 @@
+window.addEventListener("load", () => {
+    toggleImagesContainer();
+})
+
+function toggleImagesContainer() {
+    let width = document.body.clientWidth;
+    if(width >= 685) { /* under 700px */
+        let carrousselContainer = document.getElementById('carroussel-container');
+        carrousselContainer.className = 'lightbox';
+        carrousselContainer.classList.remove('carroussel');
+    }
+}
+
 function showNavBar() {
     var navMenu = document.getElementById('nav-menu');
     navMenu.style.display = 'block';
